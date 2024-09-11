1a1f5c2fcd108e0f6ea6bab0776b1487c9b5d533
Kamal Singh <kamalsingh200238@gmail.com>
2024-09-11 23:03:59 +0100

    not enrolled societies

---
+++ resources/views/user/not-enrolled.blade.php
@@ -0,0 +1,51 @@
+<x-dashboard-layout>
+    <div class="container mx-auto px-4 py-8">
+        <div id="society-tables">
+            <div class="overflow-x-auto bg-white shadow-md rounded-lg">
+                <table class="min-w-full table-auto">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Society Name
+                        </th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Society Status
+                        </th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Action
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach($notEnrolledSocieties as $society)
+                        <tr class="hover:bg-gray-50">
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm font-medium text-gray-900">{{$society->name}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <sl-tag variant="{{ $society->active ? 'success' : 'danger' }}" size="small" pill>
+                                    {{$society->active ? 'Active' : 'Not Active'}}
+                                </sl-tag>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                <sl-button
+                                    type="button"
+                                    variant="success"
+                                    size="small"
+                                    hx-get="{{route('user.join-society', ['society' => $society->id])}}"
+                                    hx-target="#society-tables"
+                                    hx-select="#society-tables"
+                                >Join
+                                </sl-button>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="mt-8" hx-boost="true" hx-replace-url="true">
+                {{ $notEnrolledSocieties->links() }}
+            </div>
+        </div>
+    </div>
+</x-dashboard-layout>
