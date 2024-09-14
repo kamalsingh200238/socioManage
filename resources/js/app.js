581b6ac4c250dae16d547b34c3ddf89551fce268
Kamal Singh <kamalsingh200238@gmail.com>
2024-09-14 13:42:20 +0100

    set base path for shoelace

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,22 @@ import './bootstrap'
 
 import htmx from 'htmx.org'
 import _hyperscript from 'hyperscript.org'
+
 import '@shoelace-style/shoelace/dist/themes/light.css'
 import '@shoelace-style/shoelace/dist/components/input/input.js'
 import '@shoelace-style/shoelace/dist/components/alert/alert.js'
 import '@shoelace-style/shoelace/dist/components/tag/tag.js'
-import '@shoelace-style/shoelace/dist/components/alert/alert.js'
 import '@shoelace-style/shoelace/dist/components/button/button.js'
 import '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js'
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js'
 import '@shoelace-style/shoelace/dist/components/menu/menu.js'
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js'
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js'
 import '@shoelace-style/shoelace/dist/components/avatar/avatar.js'
+
+import {setBasePath} from "@shoelace-style/shoelace";
+
+setBasePath('/');
 
 window.htmx = htmx
 _hyperscript.browserInit()
