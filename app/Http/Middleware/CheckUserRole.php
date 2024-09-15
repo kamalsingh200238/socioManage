abb58caec75bf6b8d6ee079138fe15484aa2aafd
Kamal Singh <kamalsingh200238@gmail.com>
2024-09-15 14:03:01 +0100

    middleware for roles

---
+++ app/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+
+        $userRole = Auth::user()->role;
+
+        foreach ($roles as $role) {
+            if ($userRole === UserRole::tryFrom($role)) {
+                return $next($request);
+            }
+        }
+
+        abort(403, 'Unauthorized action.');
+    }
+}
